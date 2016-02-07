1563feda0b1fb38a236179dc5d1c16edde019c27
Robert Schütz <rschuetz17@gmail.com>
2016-02-07 20:01:13 +0100

    Switch back to fish ;-)

--- fish/config.fish
+++ fish/config.fish
@@ -8,14 +8,23 @@ set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")   # begin standout-mode - info b
 set -xU LESS_TERMCAP_ue (printf "\e[0m")          # end underline
 set -xU LESS_TERMCAP_us (printf "\e[01;32m")      # begin underline
 
-set -x PATH $PATH (ruby -e 'print Gem.user_dir')/bin
+set -x PATH ~/bin (ruby -e 'print Gem.user_dir')/bin $PATH
 
-set -x VISUAL nvim
 set -x BROWSER firefox
-set -x PAGER most
-set -x TERMCMD urxvt # Which terminal ranger uses to start an application in
+set -x TERMCMD urxvtc # Which terminal ranger uses to start an application in
 set -x GTK_THEME Adwaita
 
-alias octave="octave --no-gui"
-alias vi="nvim"
-alias vim="nvim"
+alias octave "octave --no-gui"
+
+alias vi "vim"
+if test -x (which nvim)
+  set -x VISUAL nvim
+  alias vim "nvim"
+else
+  set -x VISUAL vim
+  alias vi "vim"
+end
+
+if test -x (which most)
+  set -x PAGER most
+end

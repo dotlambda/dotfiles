eeacebc9a6bec9a00f3bf758383f2f6de53f351d
Robert Schütz <rschuetz17@gmail.com>
2016-02-09 08:56:24 +0100

    Don't show greeting if executables are not in $PATH

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,3 +1,8 @@
-function fish_greeting
-	fortune -s | cowthink -f tux -W (math (stty size | cut -d " " -f 2)-3) | lolcat
+if test -x '(which fortune)' -a -x '(which cowthink)' -a -x '(which lolcat)'
+  function fish_greeting
+    fortune -s | cowthink -f tux -W (math (stty size | cut -d " " -f 2)-3) | lolcat
+  end
+else
+  function fish_greeting
+  end
 end
